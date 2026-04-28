e3ecb29adaef779bbb8f37aaf7031703bfdae261
shaharyar <shaharyar4t4@gmail.com>
2026-04-28 17:21:14 +0500

    initial commit

---
+++ arraybasic.java
@@ -0,0 +1,19 @@
+public class arraybasic{
+
+    public static void main(String[] args) {
+        // int[] myNumbers; // Declare
+        // myNumbers = new int[5];  // Initialize
+
+        int[] myNumber = new int[5]; // Declaration + Size Initialization
+        
+        //Insert 
+        myNumber [0] = 10;
+        myNumber [1] = 20;
+        myNumber [2] = 30;
+        myNumber [3] = 40;
+        myNumber [4] = 50;
+
+        System.out.println("Element the [2]: " + myNumber[2]);
+
+    }
+}

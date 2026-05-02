b043ee4976fde29393d6f7241c0953b4373b9bcd
shaharyarali <shaharyar4t4@gmail.com>
2026-05-02 15:56:44 +0500

    learing about the For Each loop in treversing...

--- trversingarray.java
+++ trversingarray.java
@@ -12,24 +12,22 @@ public class trversingarray {
 
         // visit the every element
         // for (int i = 0; i < number.length; i++) {
-        //     System.out.println("The Element at " +"["+i+"]"+ ": " + number[i]);
+        // System.out.println("The Element at " +"["+i+"]"+ ": " + number[i]);
         // }
 
         // visit the every element in reverse..
-    //     for(int i = number.length -1 ; i > 0; i--) // number.length -1 (means jo last index se kiya bt se reverse order me jaya)
-    //     {
-    //         System.out.println("The Element at " +"["+i+"]"+ ": " + number[i]);
-    //     }
-        
+        // for(int i = number.length -1 ; i > 0; i--) // number.length -1 (means jo last
+        // index se kiya bt se reverse order me jaya)
+        // {
+        // System.out.println("The Element at " +"["+i+"]"+ ": " + number[i]);
+        // }
+
         // Treversing by using the For Each Loop
 
         // num is store the index value like [0] =10
-        for(int num : number){
-            System.out.println("the Element:"+ num );
+        for (int num : number) {
+            System.out.println("the Element:" + num);
         }
-
-
-
 
     }
 }

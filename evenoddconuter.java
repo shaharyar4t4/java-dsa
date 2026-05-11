00f527268b61ee3d2cd558694da5c2a7b2a76711
shaharyarali <shaharyar4t4@gmail.com>
2026-05-11 17:05:01 +0500

    leaning about How we find the no of value in even or odd in Array

---
+++ evenoddconuter.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class evenoddconuter {
+
+    public static void main(String[] args) {
+        int[] number = { 2, 4, 6, 8, 3, 92, 4, 75, 93 };
+
+        evenoddconuter(number);
+
+        System.out.println("no of Element: " + Arrays.toString(number));
+    }
+
+    public static void evenoddconuter(int[] arr) {
+        int evenconuter = 0;
+        int oddconunter = 0;
+
+        for (int numbers : arr) {
+            if (numbers % 2 == 0) {
+                evenconuter++;
+            } else {
+                oddconunter++;
+            }
+        }
+
+        System.out.print("Even Element: " + evenconuter +"\n");
+        System.out.println("Odd Element: " + oddconunter);
+    }
+
+}

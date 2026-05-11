c2ce94030e92bd7fb83ae568eefa9a8fae9614d0
shaharyarali <shaharyar4t4@gmail.com>
2026-05-11 16:55:26 +0500

    leaning about the Reverse in Array

---
+++ reverseArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class reverseArray {
+
+    // 0(n) => Big Notation
+    public static void main(String[] args) {
+        int[] number = { 10, 15, 12, 17, 18, 20, 19 };
+
+        reverseArrayvalue(number);
+        System.out.println("Your Array Element: " + Arrays.toString(number));
+    }
+
+    public static void reverseArrayvalue(int[] arr) {
+        int start = 0; // [0] = 10
+        int end = arr.length - 1; // [6] = 19
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+    }
+}

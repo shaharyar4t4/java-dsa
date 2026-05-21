b41ab9ca6d4125fac429ab5229a46aea46b7cf69
shaharyarali <shaharyar4t4@gmail.com>
2026-05-22 01:53:55 +0500

    complete the learning about Reverse in String by using the different methods

--- string/reversestring.java
+++ string/reversestring.java
@@ -1,21 +1,45 @@
 package string;
-public class reversestring{
-    public static void main(String [] args){
+// public class reversestring{
+//     public static void main(String [] args){
 
-        // this is immutable === mean it is not changeable
-        String original = "Shaharyar";
-        String value = reverseString(original);
-        System.out.println("the Reverse String: "+value);
-        
+//         // this is immutable === mean it is not changeable
+//         String original = "Shaharyar";
+//         String value = reverseString(original);
+//         System.out.println("the Reverse String: "+value);
+
+//     }
+
+//    public static String reverseString(String val){
+//     StringBuilder value = new StringBuilder();
+
+//     for(int i = val.length() -1; i >= 0; i--){
+//         value.append(val.charAt(i));
+//     }
+//     return value.toString();
+//    }
+// }
+
+public class reversestring {
+    public static void main(String[] args) {
+        String name = "Shaharyar";
+
+        String res = reversvalue(name);
+
+        System.out.println("Reverice value: " + res);
     }
 
-   public static String reverseString(String val){
-    StringBuilder value = new StringBuilder();
-    
-    for(int i = val.length() -1; i >= 0; i--){
-        value.append(val.charAt(i));
+    public static String reversvalue(String str) {
+
+        // reverstemp is help to tempure store the values..
+        String reverstemp = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverstemp += str.charAt(i);
+
+            // charAt(i) means store each value in Array..
+        }
+
+        return reverstemp;
+
     }
-    return value.toString();
-   }
 }
-

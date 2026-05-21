55c6e89ee563bf290b0b345aeb043e6f5da561c0
shaharyar <shaharyar4t4@gmail.com>
2026-05-21 15:31:28 +0500

    complete the leaning of Reverse String

--- reversestring.java
+++ reversestring.java
@@ -1,7 +1,20 @@
-public class ReverseString{
+public class reversestring{
     public static void main(String [] args){
 
         // this is immutable === mean it is not changeable
         String original = "Shaharyar";
+        String value = reverseString(original);
+        System.out.println("the Reverse String: "+value);
+        
     }
+
+   public static String reverseString(String val){
+    StringBuilder value = new StringBuilder();
+    
+    for(int i = val.length() -1; i >= 0; i--){
+        value.append(val.charAt(i));
+    }
+    return value.toString();
+   }
 }
+

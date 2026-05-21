69fbb0b45e7f56901bd1e319f4c8dc09718656a4
shaharyar <shaharyar4t4@gmail.com>
2026-05-21 15:12:37 +0500

    learning half of reversestiing

---
+++ reversestring.java
@@ -0,0 +1,7 @@
+public class ReverseString{
+    public static void main(String [] args){
+
+        // this is immutable === mean it is not changeable
+        String original = "Shaharyar";
+    }
+}

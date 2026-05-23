60bddd342eaf9d25959c4a95baacaa18b3df640e
shaharyarali <shaharyar4t4@gmail.com>
2026-05-24 01:52:54 +0500

    learning about the palindrom checker

---
+++ string/palindromcheck.java
@@ -0,0 +1,32 @@
+package string;
+
+public class palindromcheck {
+    public static void main(String[] args) {
+        String word = "madam";
+
+        System.out.println("the except word: "+ palindromchecker(word));
+    }
+
+    public static boolean palindromchecker(String str){
+        int start = 0;// select start value means "move forward"
+        int end = str.length() - 1; // select end value means "move backward"
+
+        while (start < end) {
+
+            // charAt is pass the specific the index value...
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }
+
+            start ++;
+            end -- ; 
+        }
+
+        return true;
+    }
+}
+
+
+// IMPORTAMT "palindrome means: the word reads the same from left → right and right → left"
+
+// shaharyar  "a = 3" "h = 2" "r = 2"

2248651cdff76f1278e4ba0262b31e8de55acae2
shaharyarali <shaharyar4t4@gmail.com>
2026-05-26 02:18:10 +0500

    Leaning about  countcharaterwithoutspace class to count characters excluding spaces

---
+++ string/countcharaterwithoutspace.java
@@ -0,0 +1,19 @@
+package string;
+
+public class countcharaterwithoutspace {
+    public static void main(String[] args) {
+        String sen = "My name is Shaharyar";
+
+        System.out.println("the sentence become the without space " + withoutSpace(sen));
+    }
+
+    public static int withoutSpace(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                count++; // the increase the of index mean's value fetching one by one..
+            }
+        }
+        return count;
+    }
+}

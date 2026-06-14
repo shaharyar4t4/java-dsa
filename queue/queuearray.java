e539abce0c57b34c035eac7d05a9ecf575aac7b3
shaharyarali <shaharyar4t4@gmail.com>
2026-06-14 23:08:58 +0500

    Learning about queuearray class with enqueue, dequeue, peek, isEmpty, isFull, and printQueue methods

---
+++ queue/queuearray.java
@@ -0,0 +1,101 @@
+package queue;
+
+public class queuearray {
+    int[] arr;
+    int front;
+    int rear;
+    int capacity; // capacity of array
+    int count; // no of value in queue..
+
+    public queuearray(int size) {
+        arr = new int[size];
+        capacity = size; // size of array
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    public void enqueue(int item) {
+        if (isFull()) {
+            System.out.println("Queue Overflow");
+            return;
+        }
+
+        // linear queue ... we don't use this one
+        // rear++;
+
+        // linear dequeue
+        // front ++
+
+        // circular queue jese hum is ki help se enqueue ka process karya ga ...
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        count++;
+        System.out.println("Inserted: " + item);
+
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue underflow");
+            return -1;
+        }
+
+        int item = arr[front];
+        front = (front + 1) % capacity;
+        count--;
+        return item;
+    }
+
+    // return the front values..
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        return arr[front];
+    }
+
+    public boolean isEmpty() {
+        return (count == 0);
+    }
+
+    public boolean isFull() {
+        return (count == capacity);
+    }
+
+    public void printQueue() {
+
+        if (isEmpty()) {
+            System.out.println("Queue is Empty...");
+            return;
+        }
+
+        System.out.print("Queue Element: ");
+        for (int i = 0; i < count; i++) {
+            int index = (front + i) % capacity;
+            System.out.println(arr[index] + " ");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        queuearray queue = new queuearray(4);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+
+        queue.printQueue();
+
+        int removed = queue.dequeue();
+        System.out.println("Dequeue element is:" + removed);
+        queue.dequeue();
+        queue.dequeue();
+        queue.printQueue();
+
+    }
+
+}

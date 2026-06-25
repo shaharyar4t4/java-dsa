1574244f3d9b52f08cc48e7354c597492a748182
shaharyarali <shaharyar4t4@gmail.com>
2026-06-25 23:28:14 +0500

    learning about Binary tree (Inorder, Preorder & Postorder Traversing)

---
+++ tree/BinaryTree.java
@@ -0,0 +1,10 @@
+public class BinaryTree {
+    static class Node{
+        int data;
+        Node leftchild ,rightchild ;
+        Node(int data){
+            this.data = data;
+            leftchild = rightchild = null;
+        }
+    }
+}
